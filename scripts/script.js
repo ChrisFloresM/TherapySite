13721428f4f7802bc9d39897c029291cdb68c260
Christian <christian.mig.flmo@gmail.com>
2024-11-09 09:26:28 -0600

    Optimizing images

--- scripts/script.js
+++ scripts/script.js
@@ -53,27 +53,8 @@ const obs = new IntersectionObserver(function(entries) {
 
 }, {
    root: null, /* In the viewport */
-   threshold: 0.85
+   rootMargin: "-80px",
+   threshold: 0
 });
 
 obs.observe(sectionHeroElement);
-
-// Fixing flexbox gap property missing in some Safari versions
-/*
-function checkFlexGap() {
-   var flex = document.createElement("div");
-   flex.style.display = "flex";
-   flex.style.flexDirection = "column";
-   flex.style.rowGap = "1px";
-
-   flex.appendChild(document.createElement("div"));
-   flex.appendChild(document.createElement("div"));
-
-   document.body.appendChild(flex);
-   var isSupported = flex.scrollHeight === 1;
-   flex.parentNode.removeChild(flex);
-   console.log(isSupported);
-
-   if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();*/
